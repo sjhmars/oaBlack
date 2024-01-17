0d6da608da36735690cce22e51700040858f44e3
sjhmars <58546090+sjhmars@users.noreply.github.com>
2024-01-18 02:02:19 +0800

    建立自定义UserDetails

--- src/main/java/com/kunkun/oaBlack/common/config/JwtTokenStoreConfig.java
+++ src/main/java/com/kunkun/oaBlack/common/config/JwtTokenStoreConfig.java
@@ -1,14 +1,24 @@
 package com.kunkun.oaBlack.common.config;
 
+import com.kunkun.oaBlack.common.util.RedisCache;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
 public class JwtTokenStoreConfig {
     private static final String JWT_KEY = "mortal_su";
+    private static final String TOKEN_KEY = "access_token:";
+
+    @Autowired
+    private RedisCache redisCache;
 
     //    public static SecretKey generalKey(){
 //        byte[] encodedKey = Base64.getDecoder().decode(AccessTokenConfig.JWT_KEY);
@@ -17,7 +27,24 @@ public class JwtTokenStoreConfig {
 //    }
     @Bean
     public TokenStore JwtTokenStore() {
-        return new JwtTokenStore(this.jwtAccessTokenConverter());
+        return new JwtTokenStore(this.jwtAccessTokenConverter()){
+            @Override
+            public void storeAccessToken(OAuth2AccessToken token, OAuth2Authentication auth2Authentication){
+                String tokenValue = token.getValue();
+                if (token.getAdditionalInformation().containsKey("jti")){
+                    String jti = token.getAdditionalInformation().get("jti").toString();
+                    redisCache.setCacheObject(TOKEN_KEY+jti,token.getValue(),token.getExpiresIn(), TimeUnit.SECONDS);
+                }
+            }
+
+            @Override
+            public void removeAccessToken(OAuth2AccessToken token) {
+                if (token.getAdditionalInformation().containsKey("jti")){
+                    String jti = token.getAdditionalInformation().get("jti").toString();
+                    redisCache.deleteObject(TOKEN_KEY+jti);
+                }
+            }
+        };
     }
 
     @Bean
---
+++ src/main/java/com/kunkun/oaBlack/common/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.kunkun.oaBlack.common.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@AutoConfigureBefore(RedisAutoConfiguration.class)//修复会自动配置原有的RedisTemplate
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings(value = { "unchecked", "rawtypes" })
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory)
+    {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        FastJsonRedisSerializer serializer = new FastJsonRedisSerializer(Object.class);
+
+        // 使用StringRedisSerializer来序列化和反序列化redis的key值
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+
+        // Hash的key也采用StringRedisSerializer的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(serializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+}
