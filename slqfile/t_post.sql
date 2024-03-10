1ba19cdf495f1a5dc1c8d5960ca070350633a509
sjhmars <58546090+sjhmars@users.noreply.github.com>
2024-03-11 01:22:35 +0800

    post表

--- slqfile/t_post.sql
+++ slqfile/t_post.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 10/03/2024 22:35:23
+ Date: 11/03/2024 01:22:14
 */
 
 SET NAMES utf8mb4;
@@ -26,12 +26,17 @@ CREATE TABLE `t_post`  (
   `post_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '职位名字',
   `post_status` int NOT NULL COMMENT '岗位状态',
   `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `create_by_user` int NULL DEFAULT NULL COMMENT '创建人',
   PRIMARY KEY (`post_id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_post
 -- ----------------------------
-INSERT INTO `t_post` VALUES (1, '测试岗位', 1, '2024-03-10 16:43:20');
+INSERT INTO `t_post` VALUES (1, '测试岗位', 1, '2024-03-10 16:43:20', NULL);
+INSERT INTO `t_post` VALUES (2, '{\r\n    \"postName\": \"新增测试\"\r\n}', 1, '2024-03-11 01:19:35', 1);
+INSERT INTO `t_post` VALUES (3, '\"新增测试\"', 1, '2024-03-11 01:19:59', 1);
+INSERT INTO `t_post` VALUES (4, '新增测试', 1, '2024-03-11 01:20:32', 1);
+INSERT INTO `t_post` VALUES (5, '新增测试', 1, '2024-03-11 01:21:09', 1);
 
 SET FOREIGN_KEY_CHECKS = 1;
