d3a76aa5b98bfd408e4f0f37457aab5521295edd
sjhmars <58546090+sjhmars@users.noreply.github.com>
2024-03-19 08:53:57 +0800

    改了表记得导一下

--- slqfile/t_wages.sql
+++ slqfile/t_wages.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 14/03/2024 04:34:19
+ Date: 19/03/2024 08:31:14
 */
 
 SET NAMES utf8mb4;
@@ -25,6 +25,7 @@ CREATE TABLE `t_wages`  (
   `wages_id` int NOT NULL AUTO_INCREMENT,
   `basic_salary` int NULL DEFAULT NULL COMMENT '工资值',
   `user_id` int NULL DEFAULT NULL COMMENT '领工资的人',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL COMMENT '领工资的人名字',
   `create_time` datetime NULL DEFAULT NULL COMMENT '发工资日期',
   `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
   `create_user_id` int NULL DEFAULT NULL COMMENT '发工资的人',
@@ -38,5 +39,7 @@ CREATE TABLE `t_wages`  (
 -- ----------------------------
 -- Records of t_wages
 -- ----------------------------
+INSERT INTO `t_wages` VALUES (1, NULL, NULL, NULL, '2024-03-18 23:30:51', NULL, 6, NULL, NULL, 0, 0);
+INSERT INTO `t_wages` VALUES (2, 9000, 7, NULL, '2024-03-18 23:47:50', NULL, 6, 200, 200, 0, 0);
 
 SET FOREIGN_KEY_CHECKS = 1;
