503f2f13d0871a7d28352e9462fa99c7f8b0866b
sjhmars <58546090+sjhmars@users.noreply.github.com>
2024-03-30 16:11:34 +0800

    通过接口

--- src/main/java/com/kunkun/oaBlack/module/personnelManagement/controller/NoticeController.java
+++ src/main/java/com/kunkun/oaBlack/module/personnelManagement/controller/NoticeController.java
@@ -1,10 +1,14 @@
 package com.kunkun.oaBlack.module.personnelManagement.controller;
 
 import com.kunkun.oaBlack.common.util.ResultUtil;
+import com.kunkun.oaBlack.module.personnelManagement.dao.NoticeDao;
 import com.kunkun.oaBlack.module.personnelManagement.enitly.NoticeEntity;
+import com.kunkun.oaBlack.module.personnelManagement.service.NoticeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("代办模块")
 public class NoticeController {
 
+    @Autowired
+    private NoticeService noticeService;
+
     @ApiOperation("通过代办")
     @PostMapping("/agree")
-    public ResultUtil agree(){
-
-        return ResultUtil.faile("没写完");
+    public ResultUtil agree(@RequestBody NoticeDao noticeDao){
+        NoticeEntity noticeEntity = noticeService.agree(noticeDao.getNoticeId(),noticeDao.getNoticeContent());
+        if (noticeEntity!=null){
+            return ResultUtil.success("通过成功",noticeEntity);
+        }
+        return ResultUtil.faile("原因看log");
     }
 }
---
+++ src/main/java/com/kunkun/oaBlack/module/personnelManagement/dao/NoticeDao.java
@@ -0,0 +1,15 @@
+package com.kunkun.oaBlack.module.personnelManagement.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NoticeDao {
+
+    private Integer noticeId;
+
+    private String noticeContent;
+}
