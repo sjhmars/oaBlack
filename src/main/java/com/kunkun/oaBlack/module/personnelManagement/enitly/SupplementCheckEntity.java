d6f28edc8fb68cdafdb5a8260cd1bf975f9218e6
sjhmars <58546090+sjhmars@users.noreply.github.com>
2024-04-03 00:11:48 +0800

    补卡修改

--- src/main/java/com/kunkun/oaBlack/module/personnelManagement/enitly/SupplementCheckEntity.java
+++ src/main/java/com/kunkun/oaBlack/module/personnelManagement/enitly/SupplementCheckEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@TableName("SupplementCheckEntity")
+@TableName("supplement_check")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
